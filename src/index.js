23b4a53e31276f00e3428e789dafc81b52dafba3
Nathannz11 <zininmw@gmail.com>
2023-10-31 20:47:45 -0300

    index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import './index.css';
 import App from './App';
 
 
-const container = document.getElementById('root');
-const root = createRoot(container); 
-root.render(<App tab="home" />);
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
